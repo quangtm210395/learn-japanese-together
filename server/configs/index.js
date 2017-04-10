99d4fd1778810be4d0397145d7ddbe554c53426d
quangtm210395 <quangtmse04402@fpt.edu.vn>
2017-04-10 21:15:50 +0700

    create base project

---
+++ server/configs/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+module.exports = {
+    port : 6969,
+    mongoUrl : 'mongodb://127.0.0.1/learn-jp',
+    settingExpress : (app) => {
+        app.use(bodyParser.urlencoded({ extended : false}));
+
+        app.use(bodyParser.json());
+    },
+    secret: 'learn-jp'
+}
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const config = require('./configs');
+const mongoose = require('mongoose');
+
+const app = express();
+config.settingExpress(app);
+
+const routes = require('./routes')(app);
+
+mongoose.connect(config.mongoUrl, { server: { reconnectTries: Number.MAX_VALUE } });
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Mongodb connected at localhost/learn-jp')
+});
+
+const port = config.port;
+
+app.listen(port, () => {
+    console.log(`Server is running at localhost:${port}`);
+})
