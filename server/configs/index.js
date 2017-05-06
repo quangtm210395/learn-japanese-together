a7129ea833b05a2bab1a1cb5f95980106c5a67ac
phanmduong <phanminhduong@hotmail.com>
2017-05-07 00:58:21 +0700

    leanjp

--- server/configs/index.js
+++ server/configs/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 module.exports = {
-    port : 6969,
-    mongoUrl : 'mongodb://127.0.0.1/learn-jp',
+    port : process.env.PORT || 6969,
+    // mongoUrl : 'mongodb://127.0.0.1/learn-jp',
+    mongoUrl : (process.env.NODE_ENV === "production") ? 'mongodb://phanmduong:duong1997@ds133211.mlab.com:33211/learn-jp'
+                    : 'mongodb://127.0.0.1/learn-jp'
+    ,
     settingExpress : (app) => {
         app.use(bodyParser.urlencoded({ extended : false}));
 
