0af36d916b3254399220ac77d8bcb2e285f636d6
quangtm210395 <quangtmse04402@fpt.edu.vn>
2017-05-13 15:40:38 +0700

    demo 0513

--- server/configs/index.js
+++ server/configs/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 module.exports = {
     port : 6969,
+    httpsPort: 8080,
     mongoUrl : 'mongodb://127.0.0.1/learn-jp',
     settingExpress : (app) => {
         app.use(bodyParser.urlencoded({ extended : false}));
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const config = require('./configs');
 const mongoose = require('mongoose');
+const fs = require('fs');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const socketio = require('./socketio')(io);
+const https = require('https');
+
+var privateKey  = fs.readFileSync('server/fakekeys/server.key', 'utf8');
+var certificate = fs.readFileSync('server/fakekeys/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+const httpsServer = https.Server(credentials, app);
 
 config.settingExpress(app);
 
@@ -21,7 +29,12 @@ db.once('open', function () {
 });
 
 const port = config.port;
+const httpsPort = config.httpsPort;
 
 server.listen(port, () => {
     console.log(`Server is running at localhost:${port}`);
+});
+
+httpsServer.listen(httpsPort, () => {
+    console.log(`Https server is running at localhost:${httpsPort}`);
 });
