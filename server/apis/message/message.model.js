9da8e03f4aa21f71264066b5fb02833feb792270
toet251 <toet.37@gmail.com>
2017-05-20 21:50:37 +0700

    Update get message by 20

--- server/apis/message/message.model.js
+++ server/apis/message/message.model.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 
 var message = mongoose.Schema({
     content: String,
-    sender: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    receiver: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+    sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    receiver: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    conversationId: String
 }, {timestamps: true});
 
 module.exports = mongoose.model('Message', message);
